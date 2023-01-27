4f38fcf83800e1bd685714029fa46c666bdaa92a
MObrien-IDM <79864181+MObrien-IDM@users.noreply.github.com>
2023-01-27 10:11:16 -0800

    import and defaults

--- sample_scens.py
+++ sample_scens.py
@@ -9,3 +9,20 @@ Created on Fri Jan 27 10:05:56 2023
 @author: michelleob
 """
 
+debug = 0
+
+if __name__ == '__main__':
+
+    import fpsim as fp
+
+    n_agents   = [10_000, 100][debug]
+    start_year = [1980, 2010][debug]
+    end_year   = 2030
+    repeats    = [3, 1][debug]
+    year       = 2020  #intervention year for all scenarios unless otherwise specified
+    youth_ages = ['<18', '18-20']
+    
+    
+    pars = fp.pars(n_agents=n_agents, start_year=start_year, end_year=end_year)
+    
+    
