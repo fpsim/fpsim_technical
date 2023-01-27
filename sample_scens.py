e0870610ad45b0761bae7a7d7278cbe3e4a42138
MObrien-IDM <79864181+MObrien-IDM@users.noreply.github.com>
2023-01-27 10:07:48 -0800

    create file for sample scenarios

---
+++ sample_scens.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+This file contains code to replicate the example scenarios published in 
+the manuscript titled "FPsim: An agent-based model of family planning", 
+first published on XXX date at YYY journal.
+
+Created on Fri Jan 27 10:05:56 2023
+
+@author: michelleob
+"""
+
